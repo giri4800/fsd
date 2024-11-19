6011586949fabd7da0d2057525c5d79660a09943
giri4800 <girishbadiger996@gmail.com>
2024-11-19 13:13:12 +0530

    updated

--- backend/login-registration-server-node/index.js
+++ backend/login-registration-server-node/index.js
@@ -16,7 +16,7 @@ const JWT_SECRET =
   "hvdvay6ert72839289()aiyg8t87qt72393293883uhefiuh78ttq3ifi78272jbkj?[]]pou89ywe";
  
 
-  mongoose.connect('mongodb+srv://girishbadiger996:8eqnCs6Nm7q4kHX@cluster0.8kdub.mongodb.net/', {
+  mongoose.connect('mongodb+srv://girishNB:Qwe123%2B-%40@cluster0.8kdub.mongodb.net/', {
     useNewUrlParser: true,
     useUnifiedTopology: true
    })
